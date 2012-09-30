586d1d3068e874bc2245bf0a446b43fc2278ec50
chuchuross <azarasi.mo.mo@gmail.com>
2012-09-30 23:07:03 +0900

    DropboxAPIの処理を別クラスに移動させた 認証済みAPIの取得処理を実装 Dropboxの画像パスを取得するためのcommandを追加 APIExceptionを追加

--- src/com/asomal/imagegallery/domain/app/DropboxAuthActivity.java
+++ src/com/asomal/imagegallery/domain/app/DropboxAuthActivity.java
@@ -9,12 +9,11 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.asomal.imagegallery.R;
+import com.asomal.imagegallery.domain.dropbox.DropboxApiManager;
 import com.asomal.imagegallery.util.Logger;
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.session.AccessTokenPair;
-import com.dropbox.client2.session.AppKeyPair;
-import com.dropbox.client2.session.Session.AccessType;
 
 //TODO 認証処理は別クラスにもっていきたい
 /**
@@ -33,41 +32,36 @@ public class DropboxAuthActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Resources res = getResources();
-
-		AppKeyPair appkeys = new AppKeyPair(res.getString(R.string.dropbox_app_key),
-				res.getString(R.string.dropbox_app_secret));
-		AndroidAuthSession session = new AndroidAuthSession(appkeys, AccessType.APP_FOLDER);
-
-		dropboxApi = new DropboxAPI<AndroidAuthSession>(session);
-		dropboxApi.getSession().startAuthentication(DropboxAuthActivity.this);
+		dropboxApi = new DropboxApiManager(this).Authentication();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if (dropboxApi.getSession().authenticationSuccessful()) {
-			dropboxApi.getSession().finishAuthentication();
-
-			AccessTokenPair tokens = dropboxApi.getSession().getAccessTokenPair();
-
-			Resources res = getResources();
-			SharedPreferences sp = getSharedPreferences(res.getString(R.string.sp_dropbox_auth), MODE_PRIVATE);
-			Editor edit = sp.edit();
-			edit.putBoolean(res.getString(R.string.sp_key_is_autentication), true);
-			edit.putString(res.getString(R.string.sp_key_access_token), tokens.key);
-			edit.putString(res.getString(R.string.sp_key_access_token_secret), tokens.secret);
-
-			if (edit.commit()) {
-				Logger.d(TAG, "アカウント同期　token: " + tokens.key + " tokenSecret: " + tokens.secret);
-				Toast.makeText(this, "アカウントの同期が完了しました。", Toast.LENGTH_SHORT).show();
-			} else {
-				Toast.makeText(this, "アカウントの同期に失敗しました。", Toast.LENGTH_SHORT).show();
-			}
-
-			finish();
-			// TODO onActivityResultで処理させたい
-			startActivity(new Intent(this, MainActivity.class));
+		if (!dropboxApi.getSession().authenticationSuccessful()) {
+			return;
 		}
+
+		dropboxApi.getSession().finishAuthentication();
+
+		AccessTokenPair tokens = dropboxApi.getSession().getAccessTokenPair();
+
+		Resources res = getResources();
+		SharedPreferences sp = getSharedPreferences(res.getString(R.string.sp_dropbox_auth), MODE_PRIVATE);
+		Editor edit = sp.edit();
+		edit.putBoolean(res.getString(R.string.sp_key_is_autentication), true);
+		edit.putString(res.getString(R.string.sp_key_access_token), tokens.key);
+		edit.putString(res.getString(R.string.sp_key_access_token_secret), tokens.secret);
+
+		if (edit.commit()) {
+			Logger.d(TAG, "アカウント同期　token: " + tokens.key + " tokenSecret: " + tokens.secret);
+			Toast.makeText(this, "アカウントの同期が完了しました。", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "アカウントの同期に失敗しました。", Toast.LENGTH_SHORT).show();
+		}
+
+		finish();
+		// TODO onActivityResultで処理させたい
+		startActivity(new Intent(this, MainActivity.class));
 	}
 }
---
+++ src/com/asomal/imagegallery/domain/dropbox/DropboxApiManager.java
@@ -0,0 +1,73 @@
+package com.asomal.imagegallery.domain.dropbox;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+
+import com.asomal.imagegallery.R;
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
+
+/**
+ * DropboxAPIの処理を扱うクラス
+ * 
+ * @author chuross
+ * 
+ */
+public class DropboxApiManager {
+
+	Context context;
+	Resources res;
+
+	public DropboxApiManager(Context context) {
+		this.context = context;
+		res = context.getResources();
+	}
+
+	/**
+	 * 認証する
+	 * 
+	 * @return session APIセッション
+	 */
+	public DropboxAPI<AndroidAuthSession> Authentication() {
+		AppKeyPair appkeys = new AppKeyPair(res.getString(R.string.dropbox_app_key),
+				res.getString(R.string.dropbox_app_secret));
+		AndroidAuthSession session = new AndroidAuthSession(appkeys, AccessType.APP_FOLDER);
+
+		DropboxAPI<AndroidAuthSession> dropboxApi = new DropboxAPI<AndroidAuthSession>(session);
+		dropboxApi.getSession().startAuthentication(context);
+
+		return dropboxApi;
+	}
+
+	/**
+	 * 認証済みAPIを取得する
+	 * 
+	 * @return 認証済みAPI
+	 * @throws ApiException Tokenがnullの時
+	 */
+	public DropboxAPI<AndroidAuthSession> getApi() throws ApiException {
+		SharedPreferences sp = context.getSharedPreferences(res.getString(R.string.sp_dropbox_auth),
+				Context.MODE_PRIVATE);
+
+		String userToken = sp.getString(res.getString(R.string.sp_key_access_token), null);
+		String userSecret = sp.getString(res.getString(R.string.sp_key_access_token_secret), null);
+
+		if (userToken == null || userSecret == null) {
+			throw new ApiException("Token is null.");
+		}
+
+		AppKeyPair access = new AppKeyPair(res.getString(R.string.dropbox_app_key),
+				res.getString(R.string.dropbox_app_secret));
+		AndroidAuthSession session = new AndroidAuthSession(access, AccessType.APP_FOLDER);
+
+		DropboxAPI<AndroidAuthSession> dropboxApi = new DropboxAPI<AndroidAuthSession>(session);
+		AccessTokenPair tokenPair = new AccessTokenPair(userToken, userSecret);
+		dropboxApi.getSession().setAccessTokenPair(tokenPair);
+
+		return dropboxApi;
+	}
+}
